1e7847e411ac827d527fb6fbaf12a3f566737f0f
Šárka Kolářová <104999788+kremzskahorcice02@users.noreply.github.com>
2022-11-14 00:14:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,46 @@ window.addEventListener('scroll', function() {
     road.style.top = value * 0.2 + 'px';
     text.style.top = value * 0.8 + 'px';
 })
+
+$(window).on("load",function() {
+    $(window).scroll(function() {
+      var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+      $(".fade").each(function() {
+        /* Check the location of each desired element */
+        var objectBottom = $(this).offset().top + $(this).outerHeight();
+        
+        /* If the element is completely within bounds of the window, fade it in */
+        if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+          if ($(this).css("opacity")==0) {$(this).fadeTo(400,1);}
+        }
+      });
+    }).scroll(); //invoke scroll-handler on page-load
+  });
+
+
+// Get the button:
+let mybutton = document.getElementById("myBtn");
+mybutton.addEventListener('click', function() {topFunction(), false});
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    const divAbout = document.getElementById('top');
+    let top = divAbout.offsetTop;
+    let height = divAbout.offsetHeight;
+    let bottom = top + height;
+  if (document.body.scrollTop > bottom || document.documentElement.scrollTop > bottom) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    const divAbout = document.getElementById('top');
+    let top = divAbout.offsetTop;
+    document.body.scrollTop = top; // For Safari
+    document.documentElement.scrollTop = top; // For Chrome, Firefox, IE and Opera
+}
